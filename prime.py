dd98959cdf61d09bbc4766ea40c79fe6a1e61b7b
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2022-03-10 19:24:00 -0300

    fim aula 8

--- prime.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(math.sqrt(n) + 1)):
-        if n % i == 0:
-            return False
-    return True
