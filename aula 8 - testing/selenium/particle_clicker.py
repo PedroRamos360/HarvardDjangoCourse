a6dcaa45fb895eeffb42ff997529316bb0f63bf1
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2022-03-11 14:37:28 -0300

    project0

--- aula 8 - testing/selenium/particle_clicker.py
+++ aula 8 - testing/selenium/particle_clicker.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import keyboard 
+from time import sleep
 
 
 driver = webdriver.Chrome()
@@ -10,8 +11,26 @@ driver.get(uri)
 
 particle = driver.find_element_by_id('detector-events')
 
+paused = True
+
+def get_paused():
+    global paused
+    return paused
+
+def set_paused():
+    global paused
+    if paused == True:
+        paused = False
+    else:
+        paused = True
+    return paused
+
 while True:
-    particle.click()
-    if keyboard.is_pressed('q'): 
-        input('Clique enter para continuar')
+    paused = get_paused()
+    if not paused:
+        particle.click()
+    if keyboard.is_pressed('p'):
+        sleep(.2)
+        print(set_paused())
+
 
